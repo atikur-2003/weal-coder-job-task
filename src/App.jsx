b8f305e401039ceca81d6d8009272e57deae3a6d
Atikur Rahman <atikur947382@gmail.com>
2025-12-24 19:07:53 +0600

    Initialization of the task

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='text-2xl'>
+            App page
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader", 
+          "css-loader",
+          "postcss-loader", 
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+  },
+  mode: "development",
+};
