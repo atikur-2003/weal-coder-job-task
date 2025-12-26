031153a489b0ce93355a1d346c3934e67b23f2cd
Atikur Rahman <atikur947382@gmail.com>
2025-12-27 01:48:52 +0600

    made some changes at webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,32 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.jsx",
-
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     publicPath: "/",
     clean: true,
   },
-
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-
+  resolve: { extensions: [".js", ".jsx"] },
   module: {
     rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        type: "asset/resource",
-      },
+      { test: /\.jsx?$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader", "postcss-loader"] },
+      { test: /\.(png|jpe?g|gif|svg|webp)$/i, type: "asset/resource" },
     ],
   },
-
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new CopyWebpackPlugin({ patterns: [{ from: "public/images", to: "images" }] }),
   ],
-
   devServer: {
     static: path.resolve(__dirname, "public"),
     historyApiFallback: true,
     port: 3000,
     open: true,
   },
-
   mode: "development",
 };
